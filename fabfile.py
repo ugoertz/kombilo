d59dec5ff6068e1846adb9e2b3c0459f173f97eb
Ulrich Goertz <ug@geometry.de>
2012-03-27 21:22:40 +0200

    Push release to 0.7.1; fix bug in v.Viewer.quit; make reading options smarter (always use default.cfg to provide default values).

--- fabfile.py
+++ fabfile.py
@@ -24,13 +24,13 @@ def doc_as_pdf():
             local('cp sphinx.sty _build/latex/')
     with lcd('/home/ug/devel/k07-bb/doc/_build/latex/'):
         local('make all-pdf')
-        local('mv Kombilo.pdf kombilo-0.7.pdf')
+        local('mv Kombilo.pdf kombilo-0.7.1.pdf')
 
 
 def deploy_targz():
     with lcd('/home/ug/devel'):
         local('mkdir kombilo')
-        local('rm -f kombilo-0.7.tar.gz')
+        local('rm -f kombilo-0.7.1.tar.gz')
         local('hg clone k07-bb kombilo')
     with lcd('/home/ug/devel/kombilo/lk/doc'):
         local('doxygen')
@@ -51,7 +51,7 @@ def deploy_targz():
         local('rm -rf .hg')
         local('rm -f fabfile.py')
     with lcd('/home/ug/devel'):
-        local('tar cfz kombilo-0.7.tar.gz kombilo')
+        local('tar cfz kombilo-0.7.1.tar.gz kombilo')
         local('rm -rf kombilo')
 
 
