a35e008998015df410c404ce12b59df91e13a373
Ulrich Goertz <ug@geometry.de>
2016-10-02 20:57:14 +0200

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,36 +3,39 @@ import urllib2
 import glob
 from fabric.api import run, cd, local, lcd, prefix, get, put
 
-# FIXME directories below depend on version!
-
-
-# --------------- documentation
+VERSION = '0.8'
+GIT_BRANCH = 'v0.8'
+DEVEL_BASE = '/home/ug/devel'
+DEVEL_DIR = 'kombilo'
+BASE_DIR = os.path.join(DEVEL_BASE, DEVEL_DIR)
+VIRTUALENV = '/bin/bash /home/ug/.virtualenvs/k08/bin/activate'
 
 def deploy_doc():
-    with cd('/home/ug/www.u-go.net/k08-bb'):
-        run('hg pull -u')  # note: as a prerequisite, the repository must be in the right branch!
-    with cd('/home/ug/www.u-go.net/k08-bb/lk'):
-        run('swig -c++ -python libkombilo.i')
-        run('python setup.py build_ext')
-        run('cp libkombilo.py build/lib.linux-*/_libkombilo.so ../src/')
-    with cd('/home/ug/www.u-go.net/k08-bb/doc'):
-        run('rm -rf ../../dl/kombilo/doc')
-        run('sphinx-build -A ugonet_online=1 -b html . ../../dl/kombilo/doc')
-    with cd('/home/ug/www.u-go.net/k08-bb/lk/doc'):
-        run('doxygen')
-        run('rm -rf ../../../dl/libkombilo/doc')
-        run('cp -a build/html ../../../dl/libkombilo/doc')
+    with lcd('%s' % BASE_DIR):
+        local('git checkout %s' % GIT_BRANCH)
+        local('git pull')
+    with lcd('%s/lk' % BASE_DIR):
+        local('swig -c++ -python libkombilo.i')
+        with prefix(VIRTUALENV):
+            local('python setup.py build_ext')
+        local('cp -n libkombilo.py build/lib.linux-*/_libkombilo.so ../src/')
+    with prefix(VIRTUALENV):
+        with lcd('%s/doc' % BASE_DIR):
+            # run('rm -rf ../../dl/kombilo/doc')
+            local('make html')
+    with lcd('%s/lk/doc' % BASE_DIR):
+        local('doxygen')
+        #  local('rm -rf ../../../dl/libkombilo/doc')
+        #  local('cp -a build/html ../../../dl/libkombilo/doc')
 
 
 def doc_as_pdf():
-    with prefix('source /home/ug/.virtualenvs/k08-bb/bin/activate'):
-        with lcd('/home/ug/devel/k08-bb/doc'):
+    with prefix(VIRTUALENV):
+        with lcd('%s/doc' % BASE_DIR):
             local('make clean')
-            local('make latex')
-            local('cp sphinx.sty _build/latex/')
-    with lcd('/home/ug/devel/k08-bb/doc/_build/latex/'):
-        local('make all-pdf')
-        local('mv Kombilo.pdf kombilo-0.8.pdf')
+            local('make latexpdf')
+    with lcd('%s/doc/_build/latex/' % BASE_DIR):
+        local('mv Kombilo.pdf kombilo-%s.pdf' % VERSION)
 
 
 # --------------- pootle
@@ -115,96 +118,32 @@ def upload_pot(ignore_errors=False):
 # --------------- kombilo main/linux
 
 def deploy_targz():
-    with lcd('/home/ug/devel'):
-        local('mkdir kombilo')
-        local('rm -f kombilo-0.8.tar.gz')
-        local('hg clone k08-bb kombilo')
-    with lcd('/home/ug/devel/kombilo/'):
-        local('hg update default')
-    with lcd('/home/ug/devel/kombilo/lk/doc'):
+    with lcd(DEVEL_BASE):
+        local('mkdir kombilo-%s' % VERSION)
+        local('rm -f kombilo-%s.tar.gz' % VERSION)
+        local('git clone %s kombilo-%s' % (DEVEL_DIR, VERSION))
+    with lcd('%s/kombilo-%s' % (DEVEL_BASE, VERSION)):
+        local('git checkout v0.7')
+    with lcd('%s/kombilo-%s/lk/doc' % (DEVEL_BASE, VERSION)):
         local('doxygen')
 
-    with lcd('/home/ug/devel/kombilo/lk'):
+    with lcd('%s/kombilo-%s/lk' % (DEVEL_BASE, VERSION)):
         local('swig -c++ -python libkombilo.i')
-        with prefix('source /home/ug/.virtualenvs/k08-bb/bin/activate'):
+        with prefix(VIRTUALENV):
             local('python setup.py build_ext')
         local('cp libkombilo.py build/lib.linux-*/_libkombilo.so ../src/')
         local('rm -rf build')
-    with prefix('source /home/ug/.virtualenvs/k08-bb/bin/activate'):
-        with lcd('/home/ug/devel/kombilo/doc'):
+    with prefix(VIRTUALENV):
+        with lcd('%s/kombilo-%s/doc' % (DEVEL_BASE, VERSION)):
             local('make html')
-    with lcd('/home/ug/devel/kombilo'):
+    with lcd('%s/kombilo-%s' % (DEVEL_BASE, VERSION)):
         local('rm src/_libkombilo.so')
-        local('rm */*.pyc')
-        local('rm */*/*.pyc')
-        local('rm -rf .hg')
+        local('rm -f */*.pyc')
+        local('rm -f */*/*.pyc')
+        local('rm -rf .git .gitignore')
         local('rm -f fabfile.py')
-    with lcd('/home/ug/devel'):
-        local('tar cfz kombilo-0.8.tar.gz kombilo')
-        local('rm -rf kombilo')
+    with lcd('%s' % DEVEL_BASE):
+        local('tar cfz kombilo-%s.tar.gz kombilo-%s' % (VERSION, VERSION))
+        local('rm -rf kombilo-%s' % VERSION)
 
 
-# --------------- kombilo main/windows installer
-
-def win_libkombilo():
-    # copy boost
-    if not os.path.exists(r'c:\Users\ug\kombilo\lk\boost'):
-        local(r'xcopy /i /s c:\Users\ug\kombilo-util\boost_1_49_0\boost c:\Users\ug\kombilo\lk\boost ')
-
-    with lcd(r'c:\Users\ug\kombilo\lk'):
-        # clean up
-        if os.path.exists('build'):
-            local('rd /q /s build')
-
-        # SWIG
-        local(r'"c:\Program Files\swigwin-2.0.4\swig" -c++ -python libkombilo.i')
-
-        # build extension
-        local('python setup.py build_ext')
-
-        # copy
-        local(r'copy /y libkombilo.py ..\src\ ')
-        local(r'copy /y build\lib.win32-2.7\_libkombilo.pyd ..\src\ ')
-
-
-def win_py2exe():
-    # copy stuff for py2exe
-    # to create the Pmw.py file use http://pmw.cvs.sourceforge.net/viewvc/pmw/Pmw/Pmw_0_0_0/bin/bundlepmw.py?revision=1.3&view=markup
-    for f in ['sqlite3.dll', 'Pmw.py', 'PmwBlt.py', 'PmwColor.py']:
-        local('copy /y c:\\Users\\ug\\kombilo-util\\%s \\Users\\ug\\kombilo\\src\\ ' % f)
-
-    # do py2exe
-    with lcd(r'c:\Users\ug\kombilo\src'):
-        # clean up
-        if os.path.exists('dist'):
-            local('rd /q /s dist')
-        local('python setup.py py2exe')
-
-
-def win_innosetup():
-    # copy stuff for innosetup
-    # vcredist_x86.exe: http://www.microsoft.com/download/en/confirmation.aspx?id=5582
-    for f in ['vcredist_x86.exe']:
-        local('copy /y c:\\Users\\ug\\kombilo-util\\%s \\Users\\ug\\kombilo\\src\\dist\\ ' % f)
-
-    for f in ['kombilo.ico']:
-        local('copy /y c:\\Users\\ug\\kombilo\\%s \\Users\\ug\\kombilo\\src\\dist\\ ' % f)
-
-    # build docs
-    with lcd(r'c:\Users\ug\kombilo\doc'):
-        local('sphinx-build -b html . ..\src\dist\doc')
-
-    # add source archive
-    u = urllib2.urlopen('https://bitbucket.org/ugoertz/kombilo/get/default.zip')  # FIXME
-    with open('c:\Users\ug\kombilo\src\dist\kombilo-source.zip', 'wb') as f:
-        f.write(u.read())
-
-    # innosetup
-    with lcd(r'c:\Users\ug\kombilo'):
-        local(r'"c:\Program Files\Inno Setup 5\ISCC" kombilo.iss')
-
-
-def deploy_win():
-    win_libkombilo()
-    win_py2exe()
-    win_innosetup()
