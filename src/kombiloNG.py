b63cbad97f760ddea3e7e1591a21a38112621d56
Ulrich Goertz <ug@geometry.de>
2016-10-04 17:51:24 +0200

    Improve error handling when db not found

--- src/kombiloNG.py
+++ src/kombiloNG.py
@@ -1282,6 +1282,8 @@ class KEngine(object):
                 db['data'] = None
             else:
                 try:
+                    if not os.path.exists(lkGameList(os.path.join(db['name'][0], db['name'][1] + '.db'))):
+                        raise IOError
                     db['data'] = lkGameList(os.path.join(db['name'][0], db['name'][1] + '.db'))
                 except:
                     if showwarning:
