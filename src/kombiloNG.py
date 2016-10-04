ugoertz/kombilo

commit b63cbad97f760ddea3e7e1591a21a38112621d56

Improve error handling when db not found

src/kombiloNG.py
@@ -1282,6 +1282,8 @@ class KEngine(object):
                 db['data'] = None
             else:
                 try:
+                    if not os.path.exists(lkGameList(os.path.join(db['name'][0], db['name'][1] + '.db'))):
+                        raise IOError
                     db['data'] = lkGameList(os.path.join(db['name'][0], db['name'][1] + '.db'))
                 except:
                     if showwarning:

commit 1d5a77adccbc96807f7ffbab77f079c751c59e88

Fix bug introduced in b63cbad

src/kombiloNG.py
@@ -1282,7 +1282,7 @@ class KEngine(object):
                 db['data'] = None
             else:
                 try:
-                    if not os.path.exists(lkGameList(os.path.join(db['name'][0], db['name'][1] + '.db'))):
+                    if not os.path.exists(os.path.join(db['name'][0], db['name'][1] + '.db')):
                         raise IOError
                     db['data'] = lkGameList(os.path.join(db['name'][0], db['name'][1] + '.db'))
                 except: